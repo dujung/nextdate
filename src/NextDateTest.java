1e36061b80df96de31e5372d72c2de25a9c07a6d
dujung <dujung@kaist.ac.kr>
2017-11-25 15:22:49 +0900

    initial eclipse project

--- src/NextDateTest.java
+++ src/NextDateTest.java
@@ -35,139 +35,102 @@ public class NextDateTest {
 	}
 	
 	@Test
-	public void invalidDay(){
-		assertEquals("Should return \"invalid Input Date\"","invalid Input Date", date.run(1,0,2012));
+	public void tc1(){
+		assertEquals("invalid Input Date", date.run(1,0,2017));
 	}
 	
 	@Test
-	public void invalidMonthLow(){
-		assertEquals("Should return \"invalid Input Date\"","invalid Input Date", date.run(0,2,2012));
+	public void tc2(){
+		assertEquals("invalid Input Date", date.run(0,1,2017));
 	}
 	
 	@Test
-	public void invalidMonthHigh(){
-		assertEquals("Should return \"invalid Input Date\"","invalid Input Date", date.run(13,2,2012));
+	public void tc3(){
+		assertEquals("invalid Input Date", date.run(13,1,2017));
 	}
-	
+
 	@Test
-	public void invalidYearLow(){
-		assertEquals("Should return \"invalid Input Date\"","invalid Input Date", date.run(2,12,1800));
+	public void tc4(){
+		assertEquals("invalid Input Date", date.run(1,1,1800));
 	}
-	
+
 	@Test
-	public void invalidYearHigh(){
-		assertEquals("Should return \"invalid Input Date\"","invalid Input Date", date.run(2,12,2022));
+	public void tc5(){
+		assertEquals("invalid Input Date", date.run(1,1,2022));
 	}
-	
+
 	@Test
-	public void almostLastDayThirtyOne(){
-		assertEquals("Should return \"01/31/2012\"","1/31/2012", date.run(1,30,2012));
+	public void tc6(){
+		assertEquals("10/24/2017", date.run(10,23,2017));
 	}
-	
+
 	@Test
-	public void lastDayThirtyOne() {
-		assertEquals("Should return \"04/01/2012\"","4/1/2012", date.run(3,31,2012));
+	public void tc7(){
+		assertEquals("11/1/2017", date.run(10,31,2017));
 	}
-	
+
 	@Test
-	public void invalidDayThirtyOne() {
-		assertEquals("Should return \"Invalid Input Date\"","Invalid Input Date", date.run(12,32,2012));
+	public void tc8(){
+		assertEquals("11/24/2017", date.run(11,23,2017));
 	}
-	
+
 	@Test
-	public void almostLastDayThirty(){
-		assertEquals("Should return \"04/30/2012\"","4/30/2012", date.run(4,29,2012));
+	public void tc9(){
+		assertEquals("12/1/2017", date.run(11,30,2017));
 	}
-	
+
 	@Test
-	public void lastDayThirty(){
-		assertEquals("Should return \"07/01/2012\"","7/01/2012", date.run(6,30,2012));
+	public void tc10(){
+		assertEquals("Invalid Input Date", date.run(11,31,2017));
 	}
-	
+
 	@Test
-	public void almostLastDayTwentyEight(){
-		assertEquals("Should return \"02/28/2013\"","2/28/2013", date.run(2,27,2013));
+	public void tc11(){
+		assertEquals("12/24/2017", date.run(12,23,2017));
 	}
-	
+
 	@Test
-	public void lastDayTwentyEight() {
-		assertEquals("Should return \"03/01/1900\"","3/1/1900", date.run(2,28,1900));
+	public void tc12(){
+		assertEquals("1/1/2018", date.run(12,32,2017));
 	}
-	
+
 	@Test
-	public void invalidLastDayLeapYear() {
-		assertEquals("Should return \"Invalid Input Date\"","Invalid Input Date", date.run(2,30,2012));
+	public void tc13(){
+		assertEquals("Invalid Next Year", date.run(12,32,2021));
 	}
-	
+
 	@Test
-	public void almostLastDayLeapYearDividedByFourHundred() {
-		assertEquals("Should return \"02/29/2000\"","2/29/2000", date.run(2,28,2000));
+	public void tc14(){
+		assertEquals("2/24/2017", date.run(2,23,2017));
 	}
-	
+
 	@Test
-	public void almostLastDayLeapYearDividedByFourHundred111() {
-		assertEquals("Should return \"02/29/2000\"","2/29/2000", date.run(2,28,2000));
+	public void tc15(){
+		assertEquals("2/29/2000", date.run(2,28,2000));
 	}
-	
+
 	@Test
-	public void lastDayLeapYear() {
-		assertEquals("Should return \"03/01/1904\"","3/1/1904", date.run(2,29,1904));
+	public void tc16(){
+		assertEquals("3/1/2017", date.run(2,28,2017));
 	}
-	
+
 	@Test
-	public void invalidDateNotLeapYear() {
-		assertEquals("Should return \"Invalid Input Date\"","Invalid Input Date", date.run(2,29,2013));
+	public void tc17(){
+		assertEquals("3/1/2000", date.run(2,29,2000));
 	}
-	
+
 	@Test
-	public void invalidDateLeapYear() {
-		assertEquals("Should return \"Invalid Input Date\"","Invalid Input Date", date.run(2,30,2012));
+	public void tc18(){
+		assertEquals("Invalid Input Date", date.run(2,29,2017));
 	}
-	
+
 	@Test
-	public void testMay() {
-		assertEquals("Should return \"05/16/2012\"","5/16/2012", date.run(5,15,2012));
+	public void tc19(){
+		assertEquals("Invalid Input Date", date.run(2,30,2017));
 	}
-	
+
 	@Test
-	public void nextDayAugust() {
-		assertEquals("Should return 08/20/1930", "8/20/1930", date.run(8, 19, 1930));
+	public void tc20(){
+		assertEquals("7/30/2017", date.run(7,30,2017));
 	}
-	
-	@Test
-	public void invalidDayInMonth() {
-		assertEquals("Should return \"Invalid Input Date\"", "Invalid Input Date", date.run(9, 31, 1945));
-	}
-	
-	@Test
-	public void nextDayOctober() {
-		assertEquals("Should return 10/28/1845", "10/28/1845", date.run(10, 27, 1845));
-	}
-	
-	@Test
-	public void nextDayNovember() {
-		assertEquals("Should return 11/02/1999", "11/2/1999", date.run(11, 1, 1999));
-	}
-	
-	@Test
-	public void nextDayDecember() {
-		assertEquals("Should return 12/31/1999", "12/31/1999", date.run(12, 30, 1999));
-	}
-	
-	@Test
-	public void newYearsEve() {
-		System.out.println(date.run(12, 31, 1999));
-		assertEquals("Should return 01/01/2000", "1/1/2000",date.run(12, 31, 1999));
-	}
-	
-	@Test
-	public void invalidNextYear() {
-		assertEquals("Should return \"Invalid Next Year\"", "Invalid Next Year", date.run(12,31,2021));
-	}
-	
-//	@Test
-//	public void testMain() {
-//		verify(date.main(null));
-//	}
-	
 }
